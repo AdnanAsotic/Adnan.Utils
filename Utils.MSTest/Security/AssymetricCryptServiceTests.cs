6785cea83f54ff306c803025360333d20f9b87be
Adnan Asotic <adnan.asotic@gmail.com>
2016-05-08 23:16:27 +0200

    added assymetric crypting

---
+++ Utils.MSTest/Security/AssymetricCryptServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Security;
+
+namespace Utils.MSTest.Security
+{
+    [TestClass]
+    public class AssymetricCryptServiceTests
+    {
+        [TestMethod]
+        public void test_should_encrypt_value_policija_with_rsa()
+        {
+            string input = "adnan.asotic";
+            AssymetricCryptService cs = new AssymetricCryptService();
+            string encrypted = cs.Encrypt(input);
+
+            string decrypted = cs.Decrypt(encrypted);
+
+            Assert.IsTrue(decrypted == input);
+        }
+
+    }
+}
--- Utils/Security/AssymetricCryptService.cs
+++ Utils/Security/AssymetricCryptService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +10,70 @@ namespace Utils.Security
 {
     public class AssymetricCryptService
     {
-        public string Encrypt()
+        RSACryptoServiceProvider _provider = new RSACryptoServiceProvider();
+
+        public string Encrypt(string value)
         {
-            throw new NotImplementedException();
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            byte[] data = encoding.GetBytes(value);
+
+            byte[] result = Encrypt(data, _provider.ExportParameters(false), false);
+
+            return encoding.GetString(result);
         }
 
-        public string Decrypt()
+        public byte[] Encrypt(byte[] value, RSAParameters rsaInfo, bool isOAEP)
         {
-            throw new NotImplementedException();
+            try
+            {
+                byte[] encryptedData = null;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(rsaInfo);
+
+                    encryptedData = RSA.Encrypt(value, isOAEP);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        public byte[] Decrypt(byte[] value, RSAParameters rsaInfo, bool isOAEP)
+        {
+            try
+            {
+                byte[] decryptedData;
+  
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(rsaInfo);
+
+                    decryptedData = RSA.Decrypt(value, isOAEP);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
+        }
+
+        public string Decrypt(string value)
+        {
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            byte[] data = encoding.GetBytes(value);
+
+            byte[] result = Decrypt(data, _provider.ExportParameters(true), false);
+
+            return encoding.GetString(result);
         }
     }
 }
