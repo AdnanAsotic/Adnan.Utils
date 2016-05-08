0f9da7486444ca57f47fe2421f0e34e318b67a30
Adnan Asotic <adnan.asotic@gmail.com>
2016-05-08 22:51:59 +0200

    AssymetricCryptService added

---
+++ Utils/Security/AssymetricCryptService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Security
+{
+    public class AssymetricCryptService
+    {
+        public string Encrypt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decrypt()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
